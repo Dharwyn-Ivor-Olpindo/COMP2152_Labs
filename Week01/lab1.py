e01a6c6a7618a8f3b593939f251c3225f41504e4
Dharwyn-Ivor-Olpindo <olpindoivor@gmail.com>
2026-01-16 12:43:15 -0500

    lab 01 fully completed

---
+++ Week01/lab1.py
@@ -0,0 +1,17 @@
+# Sample Coding Questions 01 Week 01 
+# Dharwyn Ivor Olpindo 
+
+#Question 2 
+my_array = [1, 4, 7, 9]
+
+#Question 3 
+a = 1 
+b = 2
+c = 3 
+d = 4 
+e = (a - ((b ** c) // d)) + (a % c)
+
+#Question 5
+userAge = int(input("Enter your age: "))
+userAge = userAge + 22 
+print("Now showing the shop items filtered by age", userAge)
